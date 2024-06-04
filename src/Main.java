917d0b68741547c7897397f0079ce3a8c08f4505
Alice <alisa.malikova.00.00@mail.ru>
2024-06-04 22:51:03 +0500

    Homework assignment1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello world!");
-
-
         System.out.println("задание 1");
 int a = 700;
 System.out.println("a");
